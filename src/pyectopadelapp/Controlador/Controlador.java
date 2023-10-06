bab17e0f50cd040ad031140ad70c82c4e7a3dbc2
isard <isard@DESKTOP-JNU2BQU>
2023-10-06 13:20:55 +0200

    Merge origin/master
    Conflicts:
    	src/apppadel/AppPadel.java

--- src/pyectopadelapp/Controlador/Controlador.java
+++ src/pyectopadelapp/Controlador/Controlador.java
@@ -4,10 +4,20 @@
  */
 package pyectopadelapp.Controlador;
 
-/**
- *
- * @author isard
- */
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
 public class Controlador {
-    
+    public static void conectar()throws SQLException{
+        try{
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost/padelapp","root","");
+            JOptionPane.showMessageDialog(null,"Se ha podido establecer la conexion a la base de datos");
+        }catch(Exception e){
+                JOptionPane.showMessageDialog(null,"No se ha podido establecer la conexion a la base de datos"+e.getMessage());
+        }
+    }
 }
